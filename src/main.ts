72392215bf58584e1b9983ede304bc01b2889397
RusithTharindu <rusiththarindu2@gmail.com>
2025-04-09 00:05:52 +0530

    feat: Enhance User Management with Role-based Access and Mongoose Integration

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,14 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.use(new LoggerMiddleware().use);
-
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     transform: true,
-  //   }),
-  // );
-
-  // const config = new DocumentBuilder()
-  //   .setTitle('PassGo API')
-  //   .setDescription('API Documentation for PassGo')
-  //   .setVersion('1.0.0')
-  //   .addBearerAuth()
-  //   .build();
-
-  // const configService = app.get(ConfigService);
-
-  // const document = SwaggerModule.createDocument(app, config);
-
-  // SwaggerModule.setup('api', app, document);
-
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000", 
+    origin: 'http://localhost:3000',
     credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Authorization",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
 
   await app.listen(process.env.PORT ?? 8080);
